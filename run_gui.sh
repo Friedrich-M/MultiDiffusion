7d9a157b3181c5de59f6440b0444146442efc33c
Friedrich-M <1197293057@qq.com>
2023-09-10 09:02:58 +0000

    [version]

---
+++ run_gui.sh
@@ -0,0 +1,1 @@
+python app_gradio.py
---
+++ test.py
@@ -0,0 +1,13 @@
+import torch
+from diffusers import StableDiffusionPanoramaPipeline, DDIMScheduler
+
+model_ckpt = "stabilityai/stable-diffusion-2-base"
+scheduler = DDIMScheduler.from_pretrained(model_ckpt, subfolder="scheduler")
+pipe = StableDiffusionPanoramaPipeline.from_pretrained(
+     model_ckpt, scheduler=scheduler, torch_dtype=torch.float16
+)
+
+pipe = pipe.to("cuda")
+
+prompt = "a photo of the dolomites"
+image = pipe(prompt).images[0]
